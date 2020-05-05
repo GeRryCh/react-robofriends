b9b3aa2aca002ba802ec5d79f79947afc3532c3a
German Velibekov <german.velibekov@gmail.com>
2020-05-05 15:16:25 +0300

    adding error boundry and github pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
 class App extends React.Component {
@@ -35,7 +36,9 @@ class App extends React.Component {
         <SearchBox searchChange={this.onSearchChange} />
         {!robots.length ? <h1>Loading</h1> :
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         }
       </div>
